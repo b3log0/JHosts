a670c36fb69f353ef438f58023b2170fdd2a159c
yu.zhang <zephyrjung@126.com>
2017-09-06 20:03:44 +0800

    :construction: working on scene

--- src/main/java/org/b3log/jhosts/ContentController.java
+++ src/main/java/org/b3log/jhosts/ContentController.java
@@ -1,44 +1,27 @@
 package org.b3log.jhosts;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import io.datafx.controller.ViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.annotation.PostConstruct;
 
 @ViewController(value = "/hxml/ui/Content.fxml", title = "Material Design Example")
 public class ContentController {
     @FXML
-    private JFXListView<?> list1;
-    @FXML
-    private JFXListView<?> list2;
-    @FXML
-    private JFXListView<?> subList;
-    @FXML
-    private JFXButton button3D;
-    @FXML
-    private JFXButton collapse;
-    @FXML
-    private JFXButton expand;
-
-    private int counter = 0;
+    private JFXListView<Label> hostGroup;
 
     /**
      * init fxml when loaded.
      */
     @PostConstruct
     public void init() {
-
-        button3D.setOnMouseClicked((e) -> {
-            int val = ++counter % 2;
-            list1.depthProperty().set(val);
-            list2.depthProperty().set(val);
-        });
-
-        expand.setOnMouseClicked((e) -> list2.expandedProperty().set(true));
-        collapse.setOnMouseClicked((e) -> list2.expandedProperty().set(false));
-        list1.depthProperty().set(1);
+        hostGroup.getItems().add(new Label("Zephyr 1"));
+        hostGroup.getItems().add(new Label("Zephyr 2"));
+        hostGroup.getItems().add(new Label("Zephyr 3"));
+        hostGroup.depthProperty().set(1);
+        hostGroup.expandedProperty().set(true);
     }
-
 }

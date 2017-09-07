2078e9f103d8e55b68b1c89dde7369c4cb7c7810
yu.zhang <zephyrjung@126.com>
2017-09-07 18:47:32 +0800

    :constructin: working

--- src/main/java/org/b3log/jhosts/SideMenuController.java
+++ src/main/java/org/b3log/jhosts/SideMenuController.java
@@ -1,24 +1,6 @@
 package org.b3log.jhosts;
 
 import com.jfoenix.controls.JFXListView;
-import demos.gui.uicomponents.ButtonController;
-import demos.gui.uicomponents.CheckboxController;
-import demos.gui.uicomponents.ComboBoxController;
-import demos.gui.uicomponents.DialogController;
-import demos.gui.uicomponents.IconsController;
-import demos.gui.uicomponents.ListViewController;
-import demos.gui.uicomponents.MasonryPaneController;
-import demos.gui.uicomponents.PickersController;
-import demos.gui.uicomponents.PopupController;
-import demos.gui.uicomponents.ProgressBarController;
-import demos.gui.uicomponents.RadioButtonController;
-import demos.gui.uicomponents.SVGLoaderController;
-import demos.gui.uicomponents.ScrollPaneController;
-import demos.gui.uicomponents.SliderController;
-import demos.gui.uicomponents.SpinnerController;
-import demos.gui.uicomponents.TextFieldController;
-import demos.gui.uicomponents.ToggleButtonController;
-import demos.gui.uicomponents.TreeTableViewController;
 import io.datafx.controller.ViewController;
 import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.FlowException;
@@ -31,8 +13,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import org.apache.commons.beanutils.BeanUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -45,20 +25,20 @@ public class SideMenuController {
 
     //TODO 预设五种环境，这些host总会写入到同一个文件中，只不过右侧将对五中环境进行简化显示
     @FXML
-    @ActionTrigger("profile0")
-    private Label profile0;
+    @ActionTrigger("all")
+    private Label all;
     @FXML
-    @ActionTrigger("profile1")
-    private Label profile1;
+    @ActionTrigger("product")
+    private Label product;
     @FXML
-    @ActionTrigger("profile1")
-    private Label profile2;
+    @ActionTrigger("uat")
+    private Label uat;
     @FXML
-    @ActionTrigger("profile1")
-    private Label profile3;
+    @ActionTrigger("test")
+    private Label test;
     @FXML
-    @ActionTrigger("profile1")
-    private Label profile4;
+    @ActionTrigger("local")
+    private Label local;
     @FXML
     private JFXListView<Label> sideList;
 
@@ -92,12 +72,11 @@ public class SideMenuController {
             }).start();
         });
         Flow contentFlow = (Flow) context.getRegisteredObject("ContentFlow");
-//        bindNodeToController(checkbox, CheckboxController.class, contentFlow, contentFlowHandler);
-//        bindNodeToController(combobox, ComboBoxController.class, contentFlow, contentFlowHandler);
-//        bindNodeToController(dialogs, DialogController.class, contentFlow, contentFlowHandler);
-//        bindNodeToController(listview, ListViewController.class, contentFlow, contentFlowHandler);
-//        bindNodeToController(radiobutton, RadioButtonController.class, contentFlow, contentFlowHandler);
-
+        bindNodeToController(all, AllHostController.class, contentFlow, contentFlowHandler);
+        bindNodeToController(product, ProdHostController.class, contentFlow, contentFlowHandler);
+        bindNodeToController(uat, UatHostController.class, contentFlow, contentFlowHandler);
+        bindNodeToController(test, TestHostController.class, contentFlow, contentFlowHandler);
+        bindNodeToController(local, LocalHostController.class, contentFlow, contentFlowHandler);
     }
     private void bindNodeToController(Node node, Class<?> controllerClass, Flow flow, FlowHandler flowHandler) {
         flow.withGlobalLink(node.getId(), controllerClass);

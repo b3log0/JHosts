2aae1d2667616d84361b8da9993689b2e99fd965
O型血橙 <zephyrjung@126.com>
2017-09-23 18:05:22 +0800

    :construction: 左侧修改成列表

--- src/main/java/org/b3log/jhosts/controller/SideMenuController.java
+++ src/main/java/org/b3log/jhosts/controller/SideMenuController.java
@@ -1,118 +1,129 @@
 package org.b3log.jhosts.controller;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXMasonryPane;
-import com.jfoenix.effects.JFXDepthManager;
+import com.jfoenix.controls.JFXListView;
 import io.datafx.controller.ViewController;
+import io.datafx.controller.flow.Flow;
+import io.datafx.controller.flow.FlowException;
+import io.datafx.controller.flow.FlowHandler;
+import io.datafx.controller.flow.action.ActionTrigger;
+import io.datafx.controller.flow.context.FXMLViewFlowContext;
+import io.datafx.controller.flow.context.ViewFlowContext;
+import io.datafx.controller.util.VetoException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import org.apache.commons.io.IOUtils;
+import javafx.scene.control.Label;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Objects;
 
 @ViewController(value = "/hxml/ui/SideMenu.fxml", title = "Material Design Example")
 public class SideMenuController {
 
+    @FXMLViewFlowContext
+    private ViewFlowContext context;
     @FXML
-    private ScrollPane scrollPane;
+    @ActionTrigger("buttons")
+    private Label button;
     @FXML
-    private JFXMasonryPane masonryPane;
+    @ActionTrigger("checkbox")
+    private Label checkbox;
+    @FXML
+    @ActionTrigger("combobox")
+    private Label combobox;
+    @FXML
+    @ActionTrigger("dialogs")
+    private Label dialogs;
+    @FXML
+    @ActionTrigger("icons")
+    private Label icons;
+    @FXML
+    @ActionTrigger("listview")
+    private Label listview;
+    @FXML
+    @ActionTrigger("treetableview")
+    private Label treetableview;
+    @FXML
+    @ActionTrigger("progressbar")
+    private Label progressbar;
+    @FXML
+    @ActionTrigger("radiobutton")
+    private Label radiobutton;
+    @FXML
+    @ActionTrigger("slider")
+    private Label slider;
+    @FXML
+    @ActionTrigger("spinner")
+    private Label spinner;
+    @FXML
+    @ActionTrigger("textfield")
+    private Label textfield;
+    @FXML
+    @ActionTrigger("togglebutton")
+    private Label togglebutton;
+    @FXML
+    @ActionTrigger("popup")
+    private Label popup;
+    @FXML
+    @ActionTrigger("svgLoader")
+    private Label svgLoader;
+    @FXML
+    @ActionTrigger("pickers")
+    private Label pickers;
+    @FXML
+    @ActionTrigger("masonry")
+    private Label masonry;
+    @FXML
+    @ActionTrigger("scrollpane")
+    private Label scrollpane;
+    @FXML
+    private JFXListView<Label> sideList;
 
     /**
      * init fxml when loaded.
      */
     @PostConstruct
     public void init() {
-        String[] latests = {};
-        try {
-            InputStream inStream = SideMenuController.class.getResourceAsStream("/latest");
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(inStream, writer, StandardCharsets.UTF_8);
-            latests = writer.toString().split("\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ArrayList<Node> children = new ArrayList<>();
-        for (int i = 0; i < latests.length && i < 8; i++) {
-            StackPane child = new StackPane();
-            double width = 100;
-            child.setPrefWidth(width);
-            double height = 50;
-            child.setPrefHeight(height);
-            JFXDepthManager.setDepth(child, 1);
-            children.add(child);
-            // create content
-            StackPane header = new StackPane();
-            String headerColor = getDefaultColor(i % 12);
-            header.setStyle("-fx-background-radius: 5 5 5 5; -fx-background-color: " + headerColor);
-            VBox.setVgrow(header, Priority.ALWAYS);
-            VBox content = new VBox();
-            content.getChildren().addAll(header);
-            JFXButton button = new JFXButton();
-
-            button.setButtonType(JFXButton.ButtonType.RAISED);
-            button.setPrefHeight(height);
-
-            button.setText(latests[i]);
-            child.getChildren().addAll(content, button);
-        }
-        masonryPane.getChildren().addAll(children);
+        Objects.requireNonNull(context, "context");
+        FlowHandler contentFlowHandler = (FlowHandler) context.getRegisteredObject("ContentFlowHandler");
+        sideList.propagateMouseEventsToParent();
+        sideList.getSelectionModel().selectedItemProperty().addListener((o, oldVal, newVal) -> {
+            new Thread(()->{
+                Platform.runLater(()->{
+                    if (newVal != null) {
+                        try {
+                            contentFlowHandler.handle(newVal.getId());
+                        } catch (VetoException exc) {
+                            exc.printStackTrace();
+                        } catch (FlowException exc) {
+                            exc.printStackTrace();
+                        }
+                    }
+                });
+            }).start();
+        });
+        Flow contentFlow = (Flow) context.getRegisteredObject("ContentFlow");
+//        bindNodeToController(button, ButtonController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(checkbox, CheckboxController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(combobox, ComboBoxController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(dialogs, DialogController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(icons, IconsController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(listview, ListViewController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(treetableview, TreeTableViewController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(progressbar, ProgressBarController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(radiobutton, RadioButtonController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(slider, SliderController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(spinner, SpinnerController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(textfield, TextFieldController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(togglebutton, ToggleButtonController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(popup, PopupController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(svgLoader, SVGLoaderController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(pickers, PickersController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(masonry, MasonryPaneController.class, contentFlow, contentFlowHandler);
+//        bindNodeToController(scrollpane, ScrollPaneController.class, contentFlow, contentFlowHandler);
     }
 
-    private String getDefaultColor(int i) {
-        String color = "#FFFFFF";
-        switch (i) {
-            case 0:
-                color = "#8F3F7E";
-                break;
-            case 1:
-                color = "#B5305F";
-                break;
-            case 2:
-                color = "#CE584A";
-                break;
-            case 3:
-                color = "#DB8D5C";
-                break;
-            case 4:
-                color = "#DA854E";
-                break;
-            case 5:
-                color = "#E9AB44";
-                break;
-            case 6:
-                color = "#FEE435";
-                break;
-            case 7:
-                color = "#99C286";
-                break;
-            case 8:
-                color = "#01A05E";
-                break;
-            case 9:
-                color = "#4A8895";
-                break;
-            case 10:
-                color = "#16669B";
-                break;
-            case 11:
-                color = "#2F65A5";
-                break;
-            case 12:
-                color = "#4E6A9C";
-                break;
-            default:
-                break;
-        }
-        return color;
+    private void bindNodeToController(Node node, Class<?> controllerClass, Flow flow, FlowHandler flowHandler) {
+        flow.withGlobalLink(node.getId(), controllerClass);
     }
 }
